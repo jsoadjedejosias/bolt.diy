d75bd60d67784aa3fd24bfc0a7d3a53e29e6dffd
jsoadjedejosias <jsoadjedejosias@gmail.com>
2026-07-28 23:42:58 +0000

    Update vite.config.ts.timestamp-1770328346417-a90f095482a09.mjs

--- vite.config.ts.timestamp-1770328346417-a90f095482a09.mjs
+++ vite.config.ts.timestamp-1770328346417-a90f095482a09.mjs
@@ -11,7 +11,7 @@ dotenv.config({ path: ".env" });
 dotenv.config();
 var vite_config_default = defineConfig((config2) => {
   return {
-    define: {
+    define: {https://boltdiy-production-eb9d.up.railway.app/
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     },
     build: {
